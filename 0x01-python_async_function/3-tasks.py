a037b75ea8de50c250a569327df63d3e4699bebb
Ngarotosto <carolvsreyesz@gmail.com>
2023-12-12 03:30:50 +0300

    add: task_wait_random takes int max_delay as input, returns asyncio.Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""task_wait_random takes int max_delay as input
+returns asyncio.Task.
+"""
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Returns a asyncio.Task"""
+    return asyncio.create_task(wait_random(max_delay))
