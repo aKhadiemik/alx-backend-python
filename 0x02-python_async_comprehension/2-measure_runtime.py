946773718740a81dcc763543252b6c4398deba0f
Ngarotosto <carolvsreyesz@gmail.com>
2023-12-14 05:15:44 +0300

    add: measure_runtime measures total runtime and returns it.

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+measure_runtime executes async_comprehension
+four times in parallel using asyncio.gather.
+
+measure_runtime measures total runtime and returns it.
+"""
+
+
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the total runtime"""
+    start_time = time.time()
+
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+
+    end_time = time.time()
+    return end_time - start_time
